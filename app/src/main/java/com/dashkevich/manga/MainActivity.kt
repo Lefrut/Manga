39ea2f52aaff8ad1c6ad44d1979ee5216fcce300
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-03-27 15:57:07 +0300

    System interface settings are completed: - Changed some theme parameters, added code to MainActivity. - Now the system design of the application coincides with the desired one.

--- app/src/main/java/com/dashkevich/manga/MainActivity.kt
+++ app/src/main/java/com/dashkevich/manga/MainActivity.kt
@@ -2,10 +2,17 @@ package com.dashkevich.manga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val wic = WindowInsetsControllerCompat(window, window.decorView)
+        wic.isAppearanceLightStatusBars = true
+        wic.isAppearanceLightNavigationBars = true
     }
 }

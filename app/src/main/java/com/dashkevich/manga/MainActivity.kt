11fcaf7cc2b6da1adb983f3666748550eed94806
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-03-28 12:09:17 +0300

    The foundation for further navigation has been created: - basically, only MainActivty was affected, hence it served as the main container for further screens - this foundation will help you to build your code conveniently and accurately

--- app/src/main/java/com/dashkevich/manga/MainActivity.kt
+++ app/src/main/java/com/dashkevich/manga/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +19,8 @@ class MainActivity : AppCompatActivity() {
         val wic = WindowInsetsControllerCompat(window, window.decorView)
         wic.isAppearanceLightStatusBars = true
         wic.isAppearanceLightNavigationBars = true
+
+        navController = (supportFragmentManager.findFragmentById(R.id.root_container) as NavHostFragment).navController
+        navController.setGraph(com.dashkevich.navigation.R.navigation.root_nav_graph)
     }
 }

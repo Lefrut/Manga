8b2aa1a24f38f366faf0ae6782aa16e2bac1f552
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-03-30 14:49:57 +0300

    Image have account TextView created - added color change code to Sign in fragment

--- features/signin/src/main/java/com/dashkevich/signin/SignInFragment.kt
+++ features/signin/src/main/java/com/dashkevich/signin/SignInFragment.kt
@@ -1,18 +1,19 @@
 package com.dashkevich.signin
 
 import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.View
-import com.dashkevich.signin.databinding.FragmentSignInBinding
 import android.widget.TextView
+import com.dashkevich.signin.databinding.FragmentSignInBinding
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
+import androidx.core.text.color
 
 
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
@@ -22,7 +23,7 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSignInBinding.bind(view)
-        //binding.haveAccount.setText()
+        binding.haveAccount.setHaveAccountText()
     }
 
     @ColorInt
@@ -35,17 +36,14 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
         return typedValue.data
     }
 
-    private fun TextView.setText(){
-        val haveAccountString = "Have account?" + " " + "Sign in!"
-        val spannable = SpannableString(haveAccountString)
-        val color = context.getColorFromAttr(com.google.android.material.R.attr.colorSecondary)
-        spannable.setSpan(
-            ForegroundColorSpan(color),
-            haveAccountString.length,
-            (haveAccountString + "Sign in!").length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
+    private fun TextView.setHaveAccountText(){
+        val haveAccountColor =
+            requireActivity().getColorFromAttr(com.google.android.material.R.attr.colorSecondaryVariant)
+        val haveAccountText = SpannableStringBuilder()
+            .append("Have account?")
+            .append(" ")
+            .color(haveAccountColor) { append("Sign in!") }
+        text = haveAccountText
     }
-
 
 }

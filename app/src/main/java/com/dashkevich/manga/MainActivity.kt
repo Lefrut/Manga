806de5ad0c140e74fb7edcd95590a0f412866a3d
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-04-03 16:58:31 +0300

    update theme and system ui - Added code to main activity, for transparent status bar and added new colors to the theme

--- app/src/main/java/com/dashkevich/manga/MainActivity.kt
+++ app/src/main/java/com/dashkevich/manga/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.dashkevich.manga
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +17,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configSystemUI()
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        val wic = WindowInsetsControllerCompat(window, window.decorView)
-        wic.isAppearanceLightStatusBars = true
-        wic.isAppearanceLightNavigationBars = true
-
-        navController = (supportFragmentManager.findFragmentById(R.id.root_container) as NavHostFragment).navController
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.root_container) as NavHostFragment).navController
         navController.setGraph(com.dashkevich.navigation.R.navigation.root_nav_graph)
     }
+
+
+    private fun configSystemUI() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val wic = WindowInsetsControllerCompat(window, window.decorView)
+
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                wic.isAppearanceLightStatusBars = true
+                wic.isAppearanceLightNavigationBars = true
+            }
+            Configuration.UI_MODE_NIGHT_YES -> {
+                //If don't have night theme
+                wic.isAppearanceLightStatusBars = true
+                wic.isAppearanceLightNavigationBars = true
+            }
+        }
+    }
+
 }
